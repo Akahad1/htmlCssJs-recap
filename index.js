0480aa49fdb1d10bf99452f44b19c919fb01f7fd
Akahad1 <aksahad234567@gmail.com>
2024-02-08 16:26:36 +0600

    3rd commit

---
+++ index.js
@@ -0,0 +1,39 @@
+// string to number
+const num='10'
+console.log(typeof Number(num))
+
+// kibaber number badate hoy
+var number =12;
+number+=10
+console.log(number)
+
+// dosomic yer pore koto nibo 
+const apple =23.9876
+var sum=apple.toFixed(3)
+console.log(sum)
+
+// index
+const data =[23,56,98,789,89]
+console.log(data[4])
+
+// number chage kora
+const data1 =[23,56,98,789,89]
+data1[1]=12
+console.log(data1)
+
+// indexof
+const data2 =[23,56,98,789,89]
+console.log(data2.indexOf(98))
+
+// while loop
+
+var bat =0;
+while(bat<2){
+    console.log('bat den Please')
+    bat++
+}
+
+// for loop
+for(var bat=0; bat<5;bat++){
+    console.log('bat den vai')
+}
